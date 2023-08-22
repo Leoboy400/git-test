e01d1ad1a082cc9b529789fbca1840c2a28d3c88
edgar <leoboy400@gmail.com>
2023-08-22 14:13:38 +0300

    newOne

---
+++ app.js
@@ -0,0 +1,34 @@
+// function counter() {
+//     const button = document.querySelector('button');
+//     const countDiv = document.querySelector('.count div');
+
+//     function count() {
+//         let i = 0;
+//         return function () {
+//             ++i;
+//             countDiv.textContent = i;
+//         }
+
+//     }
+//     button.addEventListener('click', count());
+// }
+// counter()
+
+// function calc() {
+//     let rtop = 0.3333;
+//     let left = 0;
+
+//     return {
+//         rtop: rtop,
+//         left: left
+//     }
+// }
+
+// const { rtop, left } = calc(22, 1);
+
+console.log('dsfsd');
+
+
+
+
+// 
